598e9384e3df8b5c2c07f38ffb114755d49f3e33
vietnguyen2000 <viet.nguyen.2000@hcmut.edu.vn>
2021-05-19 21:11:51 +0700

    add detect all file in folder

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import os
 
 def main():
     if (len(sys.argv) < 2): return
-    detect_button(os.path.join(os.curdir, 'images', sys.argv[1]))
+    path = os.path.join(os.curdir, sys.argv[1])
+    if (os.path.isfile(path)):
+        detect_button(path)
+    
+    elif (os.path.isdir(path)):
+        for entry in os.scandir(path):
+            if (entry.path.endswith('jpg') or entry.path.endswith('png')):
+                detect_button(entry.path)
 
 
 
